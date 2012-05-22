0f39da5e592a6be661af5ce268c3efd1f37c05d0
AidaKadyrova <a.r.kadyrova@gmail.com>
2012-05-23 01:14:02 +0400

    added comments in hw7task2

--- hw7/task2/uniqueList.h
+++ hw7/task2/uniqueList.h
@@ -1,6 +1,11 @@
 #pragma once
 #include "pointer.h"
 
+/**
+ * @class усовершенствованный класс-список,
+ * в который нельзя добавлять уже существующий элемент.
+ * при попытке удаления несуществующего бросается исключние
+ */
 class UniqueList : public Pointer
 {
 public:
@@ -18,13 +23,13 @@ public:
             throw RemoveMissingItemExpression();
         else deleteElement(n);
     }
-
+    /// класс-исключение для удаление несуществующего элемента
     class RemoveMissingItemExpression
     {
     public:
         RemoveMissingItemExpression(){}
     };
-
+    /// класс-исключение для добавления элемента, содеражащегося в списке
     class AlreadyExistItemException
     {
     public:
